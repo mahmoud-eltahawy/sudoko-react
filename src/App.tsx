cbe70939b54ea2ac5ebb3324484d1443089ad5e0
mahmoud-eltahawy <gemmy9300@gmail.com>
2024-08-05 15:03:58 +0300

    win function

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,38 @@ export const SIZE = 9;
 const success = "bg-lime-950"
 const failure= "bg-red-950"
 
-function App() {
-  const [rows,set_rows] = useState(new Uint8Array(SIZE * SIZE).fill(0))
 
-  let is_it = success; 
+function App() {
+  const [board,set_board] = useState(new Uint8Array(SIZE * SIZE).fill(0))
+
+  let is_valid = success; 
+
+  const game_ended = () => {
+    if (!is_valid) {
+      return false;
+    }
+
+    for (const x of board) {
+      if (!x) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
   try {
-    is_it = is_valid_sudoku(rows!) ? success : failure;
+    is_valid = is_valid_sudoku(board) ? success : failure;
   } catch(_e) {}
 
   return (
-      <BoardContext.Provider value={rows}>
-        <BoardContextSetter.Provider value={set_rows}>
-        <main className={'h-screen w-screen flex flex-rows-1 gap-5 place-content-center p-5 ' + is_it}>
+      <BoardContext.Provider value={board}>
+        <BoardContextSetter.Provider value={set_board}>
+        <main className={'h-screen w-screen flex flex-rows-1 gap-5 place-content-center p-5 ' + is_valid}>
           <Board/>
+          {
+              game_ended() ? "you win" : ""
+          }
         </main>
         </BoardContextSetter.Provider>
       </BoardContext.Provider>
