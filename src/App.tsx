663187fd5f44d05ade346c70162961b261b4322e
mahmoud-eltahawy <gemmy9300@gmail.com>
2024-08-05 13:23:23 +0300

    drop bun && fix wasm issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { useState} from 'react'
 import { BoardContext, BoardContextSetter } from './BoardContext';
 import { Board } from './Board';
-// import {is_valid_sudoku} from 'blazing_fast'
+import {is_valid_sudoku} from 'blazing-fast'
 
 export const SIZE = 9; 
 
 function App() {
   const [rows,set_rows] = useState(new Uint8Array(SIZE * SIZE).fill(0))
+  const is_it = is_valid_sudoku(rows);
   return (
       <BoardContext.Provider value={rows}>
         <BoardContextSetter.Provider value={set_rows}>
         <div className='flex flex-rows-1 place-content-center p-5'>
           <Board/>
+          <button>{is_it ? "true" :"false"}</button>
         </div>
         </BoardContextSetter.Provider>
       </BoardContext.Provider>
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import init from 'blazing-fast'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+init()
+  .then(() => {
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    )
+
+  })
+  .catch(e => console.log("Error ",e))
+
