ac028206c3de8bb600ddda4d6070ba019c02e0c8
mahmoud-eltahawy <gemmy9300@gmail.com>
2024-08-07 23:03:07 +0300

    style winning modal

--- src/WinModal.tsx
+++ src/WinModal.tsx
@@ -13,12 +13,17 @@ export function WinModal({ on_next_click, show} : WinModalParams) {
     return (
         <div
             role='dialog'
-            className='bg-white grid grid-cols-1 size-96 text-center text-4xl rounded-lg border-4 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-            <h2 className='m-5 p-5'>you win 🏆</h2>
+            className='bg-slate-500 grid grid-cols-1 size-96 text-center rounded-lg absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+            <h2 className="m-5 p-5 text-5xl">
+                <span className='bg-gradient-to-r from-blue-400 via-red-400 to-yellow-300 text-transparent bg-clip-text'>you win </span>
+                <span>🏆</span>
+            </h2>
             <button
                 onClick={on_next_click}
-                className='border-2 rounded-full'
-            >next level</button>
+                className='border-2 rounded-full text-4xl m-5 hover:text-5xl border-yellow-500 hover:border-blue-500'
+            >
+                <span className='bg-gradient-to-r from-yellow-400 via-red-400 to-blue-300 text-transparent bg-clip-text'>next level</span>
+            </button>
         </div>
     );
 }
